8f9a005d8b73d2b1c82780ea03a2ae794c35ec3d
jungkyu <jungkyu.yi@channeltune.net>
2022-11-29 17:15:30 +0900

    data get

--- app/src/main/java/com/example/droi_mvvm/Const.kt
+++ app/src/main/java/com/example/droi_mvvm/Const.kt
@@ -2,7 +2,8 @@ package com.example.droi_mvvm
 
 class Const {
     companion object {
-        const val BASE_URL: String = "https://jpassets.jobplanet.co.kr/"
+        //        const val BASE_URL: String = "https://jpassets.jobplanet.co.kr/"
+        const val BASE_URL = BuildConfig.SERVER_URL
         const val APP_CACHE: String = "jk_cache"
     }
 }
